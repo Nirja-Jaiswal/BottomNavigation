11cafba030f44201fcb3105b50a6b0f022d24f04
Nirja <nirjaiswal@gmail.com>
2022-08-08 12:22:35 +0530

    Floating Buttom Bottom navigation

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.bottomnavigation;
+
+import android.os.Bundle;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+    BadgeDrawable badge;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.view);
+        badge=navView.getOrCreateBadge(R.id.navigation_dashboard);
+        badge.setNumber(1000);
+        badge=navView.getOrCreateBadge(R.id.navigation_notifications);
+        badge.setNumber(1000);
+
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_setting)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
